b7757fd49fc1c6c87d64a7748eaa389b2fd91eed
Ken <ken.su_dev@hotmail.com>
2023-07-11 15:55:32 +0200

    Initial commit

---
+++ app/src/main/java/com/suonk/caen_festival/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.suonk.caen_festival
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
